93b797cd9e60db82d6fd7d3ac478ae9b109144d1
Enzo Paiva <enzodouglaspaiva@outlook.com>
2024-07-03 22:04:40 -0300

    feat/TDS-58-export-pdf-excel-plans

--- src/plan/plan.controller.ts
+++ src/plan/plan.controller.ts
@@ -6,11 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  Res,
+  NotFoundException,
 } from '@nestjs/common';
 import { PlanService } from './plan.service';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { PlanQueryDto } from './dto/plan-query.dto';
+import { Response } from 'express';
 
 @ApiTags('plan')
 @Controller('plan')
@@ -40,5 +45,77 @@ export class PlanController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.planService.remove(+id);
+  }
+
+  @Get('export/excel')
+  async exportToExcel(@Query() queryParams: PlanQueryDto, @Res() res: Response) {
+    try {
+      const { buffer, foundPlans } = await this.planService.exportToExcel(queryParams);
+
+      if (!foundPlans) {
+          // Lança NotFoundException se nenhum plano foi encontrado
+          throw new NotFoundException('Plan not found');
+      }
+
+      res.set({
+          'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          'Content-Disposition': `attachment; filename="plan_${Date.now()}.xlsx"`,
+      });
+
+      res.send(buffer); // Envia o buffer como resposta
+
+    } catch (error) {
+        if (error instanceof NotFoundException) {
+            // Envia 404 para NotFoundException
+            res.status(404).send({
+                statusCode: 404,
+                message: error.message,
+            });
+        } else {
+            // Loga outros erros e envia resposta 500
+            console.error(error);
+            res.status(500).send({
+                statusCode: 500,
+                message: 'Internal server error',
+            });
+        }
+    }
+  }
+
+  @Get('export/pdf')
+  async exportToPDF(@Query() queryParams: PlanQueryDto, @Res() res: Response) {
+    try {
+        const { buffer, foundPlans } = await this.planService.exportToPDF(queryParams);
+
+        console.log(buffer, foundPlans)
+        if (!foundPlans) {
+            // Lança NotFoundException se nenhum plano foi encontrado
+            throw new NotFoundException('Plans not found');
+        }
+
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': `attachment; filename="plan_${Date.now()}.pdf"`,
+            'Content-Length': buffer.length,
+        });
+
+        res.send(buffer); // Envia o buffer como resposta
+
+    } catch (error) {
+        if (error instanceof NotFoundException) {
+            // Envia 404 para NotFoundException
+            res.status(404).send({
+                statusCode: 404,
+                message: error.message,
+            });
+        } else {
+            // Loga outros erros e envia resposta 500
+            console.error(error);
+            res.status(500).send({
+                statusCode: 500,
+                message: 'Internal server error',
+            });
+        }
+    }
   }
 }
