cfb5a0fa635f579a18b96aeb815a3160e54303b0
Enzo Paiva <enzodouglaspaiva@outlook.com>
2024-07-05 17:05:52 -0300

    TDS-110 - update swagger plans

--- src/plan/dto/plan-query.dto.ts
+++ src/plan/dto/plan-query.dto.ts
@@ -1,20 +1,25 @@
 import { IsOptional, IsNumber, IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PlanQueryDto {
+  @ApiProperty({ required: false, description: 'Name of the plan' })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   name?: string;
 
+  @ApiProperty({ required: false, description: 'Description of the plan' })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   description?: string;
 
+  @ApiProperty({ required: false, description: 'Minimum price of the plan' })
   @IsOptional()
   @IsNumber()
   minPrice?: number;
 
+  @ApiProperty({ required: false, description: 'Maximum price of the plan' })
   @IsOptional()
   @IsNumber()
   maxPrice?: number;
