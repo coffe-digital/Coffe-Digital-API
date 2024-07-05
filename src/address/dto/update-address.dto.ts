e76d2f0711464d1c8424eaab7c1b68386fd2d9fe
Enzo Paiva <enzodouglaspaiva@outlook.com>
2024-07-05 17:02:50 -0300

    TDS-110 - update swagger address

--- src/address/dto/update-address.dto.ts
+++ src/address/dto/update-address.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateAddressDto } from './create-address.dto';
 
-export class UpdateAddressDto extends PartialType(CreateAddressDto) {}
+export class UpdateAddressDto extends PartialType(CreateAddressDto) {
+  @ApiProperty({ description: 'State of the address' })
+  state: string;
+
+  @ApiProperty({ description: 'City of the address' })
+  city: string;
+
+  @ApiProperty({ description: 'Street name' })
+  street: string;
+
+  @ApiProperty({ description: 'House or apartment number' })
+  number: string;
+
+  @ApiProperty({ description: 'Postal code' })
+  postal_code: string;
+
+  @ApiProperty({ description: 'Additional description of the address' })
+  description: string;
+
+  @ApiProperty({ description: 'ID of the client associated with this address' })
+  clientId: number;
+}
